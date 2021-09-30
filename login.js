32ff1c10e72463c7ee71a58d10dd9538141e64cd
Ch10934523 <saibhuvanesh.c@innomick.com>
2021-09-30 19:36:25 +0530

    log in page password validation

--- login.js
+++ login.js
@@ -2,31 +2,6 @@ let username1 = document.getElementById("username");
 let password1 = document.getElementById("password1");
 let loginbtn = document.getElementById("loginbutton");
 
-
-
-
-loginbtn.onclick = function() {
-
-    const details = {
-
-        email: username1.value,
-        password : password1.value
-    };
-
-
-
-    let stringifiedvalue = JSON.stringify(details);
-
-    console.log(stringifiedvalue);
-
-    let obj1 = new XMLHttpRequest();
-
-    obj1.onload = function() {
-        console.log(JSON.parse(this.responseText));
-    }
-
-    obj1.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-    
-    obj1.send();
-
+loginbtn.onclick = () => {
+    alert("You entered a valid text");
 }
